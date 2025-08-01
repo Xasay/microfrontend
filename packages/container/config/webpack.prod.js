8400bd0fd2838c57a508c7370f663056611470d5
Khasay Guliyev <khguliyev@rth.gov.az>
2025-08-01 10:43:45 +0400

    Created container deploy script

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,25 @@
+const {merge} = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common.js');
+const packageJson = require('../package.json');
+
+const domain = process.env.DOMAIN ;
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+    },
+    plugins:[
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes:{
+                //butun proyektler eyni domain uzerinden istidam edilecek . Amma muxtelif folderlerde 
+                marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+            },
+            shared: packageJson.dependencies,
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, prodConfig);
