8784f01f4edffde6b6c2cff46132168afd42eff5
Khasay Guliyev <khguliyev@rth.gov.az>
2025-08-03 12:30:46 +0400

    webpack prod publicpath config

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,25 +1,27 @@
-const {merge} = require('webpack-merge');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common.js');
-const packageJson = require('../package.json');
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common.js");
+const packageJson = require("../package.json");
 
-const domain = process.env.DOMAIN ;
+const domain = process.env.DOMAIN;
 
 const prodConfig = {
-    mode: 'production',
-    output: {
-        filename: '[name].[contenthash].js',
-    },
-    plugins:[
-        new ModuleFederationPlugin({
-            name: 'container',
-            remotes:{
-                //butun proyektler eyni domain uzerinden istidam edilecek . Amma muxtelif folderlerde 
-                marketing: `marketing@${domain}/marketing/remoteEntry.js`,
-            },
-            shared: packageJson.dependencies,
-        })
-    ]
-}
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+    //bu publicPath gosterir ki deploy olunan main.js file hansi folderlerin altinda yerlesecek
+    publicPath: "/conatiner/latest/",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        //butun proyektler eyni domain uzerinden istidam edilecek . Amma muxtelif folderlerde
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
 
 module.exports = merge(commonConfig, prodConfig);
